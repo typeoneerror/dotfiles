caa70855dee332540d4160df67bffefc5ec9cb80
Benjamin Borowski <ben@weareokidoki.com>
2017-06-01 17:06:46 -0700

    Link nvim init script

--- link.sh
+++ link.sh
@@ -1,5 +1,4 @@
 ln -sv ~/dotfiles/vim/vimrc ~/.vimrc
-ln -sv ~/dotfiles/vim/gvimrc ~/.gvimrc
 
 ln -sv ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sv ~/dotfiles/git/gitignore ~/.gitignore
@@ -7,3 +6,6 @@ ln -sv ~/dotfiles/git/gitignore ~/.gitignore
 ln -sv ~/dotfiles/shell/bash_profile ~/.bash_profile
 
 ln -sv ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config/nvim
+ln -sv ~/.vimrc ~/.config/nvim/init.vim
