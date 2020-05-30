26fb3fa236e7fc5d3c1827be37c34d63a8a0d169
Benjamin Borowski <ben@weareokidoki.com>
2020-05-30 16:43:35 -0700

    bash: sets editing-mode to vi

--- link.sh
+++ link.sh
@@ -4,6 +4,7 @@ ln -sv ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sv ~/dotfiles/git/gitignore ~/.gitignore
 
 ln -sv ~/dotfiles/shell/bash_profile ~/.bash_profile
+ln -sv ~/dotfiles/shell/inputrc ~/.inputrc
 
 ln -sv ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
