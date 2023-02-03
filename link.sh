81c52c1c24f231bfec40d87a0984ea48d186ee71
Benjamin Borowski <ben@weareokidoki.com>
2023-02-02 22:51:39 -0800

    (zsh) sets up a basic zsh shell

--- link.sh
+++ link.sh
@@ -12,7 +12,10 @@ ln -sv ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sv ~/dotfiles/git/gitignore ~/.gitignore
 
 ln -sv ~/dotfiles/shell/bash_profile ~/.bash_profile
+ln -sv ~/dotfiles/shell/zsh/zprofile ~/.zprofile
+ln -sv ~/dotfiles/shell/zsh/zshrc ~/.zshrc
 ln -sv ~/dotfiles/shell/inputrc ~/.inputrc
+ln -sv ~/dotfiles/shell/hushlogin ~/.hushlogin
 
 ln -sv ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
